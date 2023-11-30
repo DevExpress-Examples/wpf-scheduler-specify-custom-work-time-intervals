dc551c33c2e95aaaf51108efdb5b699065bdce42
DevExpressExampleBot <fakeEmail>
2023-11-30 16:07:09 +0400

    Source auto update [skip ci]

--- VB/CustomWorkTimeExample/MainWindow.xaml.vb
+++ VB/CustomWorkTimeExample/MainWindow.xaml.vb
@@ -2,13 +2,9 @@ Imports DevExpress.Mvvm
 Imports System
 Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Controls
 
 Namespace CustomWorkTimeExample
 
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Public Partial Class MainWindow
         Inherits Window
 
@@ -16,14 +12,10 @@ Namespace CustomWorkTimeExample
             Me.InitializeComponent()
         End Sub
 
-#Region "#CustomWorkTime"
         Private Sub SchedulerControl_CustomWorkTime(ByVal sender As Object, ByVal e As DevExpress.Xpf.Scheduling.CustomWorkTimeEventArgs)
             If e.Resource.Id.Equals("1") Then
                 If e.Interval.Start.Day Mod 2 = 0 Then
-                    Dim workTimes As List(Of TimeSpanRange) = New List(Of TimeSpanRange)()
-                    workTimes.Add(New TimeSpanRange(TimeSpan.FromHours(0), TimeSpan.FromHours(3)))
-                    workTimes.Add(New TimeSpanRange(TimeSpan.FromHours(5), TimeSpan.FromHours(8)))
-                    workTimes.Add(New TimeSpanRange(TimeSpan.FromHours(10), TimeSpan.FromHours(11)))
+                    Dim workTimes As List(Of TimeSpanRange) = New List(Of TimeSpanRange) From {New TimeSpanRange(TimeSpan.FromHours(0), TimeSpan.FromHours(3)), New TimeSpanRange(TimeSpan.FromHours(5), TimeSpan.FromHours(8)), New TimeSpanRange(TimeSpan.FromHours(10), TimeSpan.FromHours(11))}
                     e.WorkTimes = workTimes
                 Else
                     e.WorkTime = New TimeSpanRange(TimeSpan.FromHours(8), TimeSpan.FromHours(20))
@@ -40,12 +32,9 @@ Namespace CustomWorkTimeExample
 
             If e.Resource.Id.Equals("3") Then e.WorkTime = New TimeSpanRange(TimeSpan.FromHours(14), TimeSpan.FromHours(21))
             If e.Resource.Id.Equals("4") Then
-                Dim workTimes As List(Of TimeSpanRange) = New List(Of TimeSpanRange)()
-                workTimes.Add(New TimeSpanRange(TimeSpan.FromHours(8), TimeSpan.FromHours(11)))
-                workTimes.Add(New TimeSpanRange(TimeSpan.FromHours(13), TimeSpan.FromHours(17)))
+                Dim workTimes As List(Of TimeSpanRange) = New List(Of TimeSpanRange) From {New TimeSpanRange(TimeSpan.FromHours(8), TimeSpan.FromHours(11)), New TimeSpanRange(TimeSpan.FromHours(13), TimeSpan.FromHours(17))}
                 e.WorkTimes = workTimes
             End If
         End Sub
-#End Region  ' #CustomWorkTime
     End Class
 End Namespace
